cb0ad554b580d0afb2a7e7308b3c8f97af120f8e
DanielaCalisaya <hdaniela127c@gmail.com>
2022-04-12 00:00:27 -0500

    components in App

---
+++ giphy-app/src/components/Gif.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+const PageContent = () => {
+  return (
+    /* Page Content */
+    <div className="container">
+  
+      {/* Page Features */}
+      <div className="row text-center">
+  
+        <div className="col-lg-3 col-md-6 mb-4">
+          <div className="card h-100">
+            <img className="card-img-top" src="" alt=""/>
+            <div className="card-body">
+              <h4 className="card-title">Titulo</h4>
+            </div>
+          </div>
+        </div>
+  
+        <div className="col-lg-3 col-md-6 mb-4">
+          <div className="card h-100">
+            <img className="card-img-top" src="" alt=""/>
+            <div className="card-body">
+              <h4 className="card-title">Titulo</h4>
+            </div>
+          </div>
+        </div>
+  
+        <div className="col-lg-3 col-md-6 mb-4">
+          <div className="card h-100">
+            <img className="card-img-top" src="" alt=""/>
+            <div className="card-body">
+              <h4 className="card-title">Titulo</h4>
+            </div>
+          </div>
+        </div>
+  
+        <div className="col-lg-3 col-md-6 mb-4">
+          <div className="card h-100">
+            <img className="card-img-top" src="" alt=""/>
+            <div className="card-body">
+              <h4 className="card-title">Titulo</h4>
+            </div>
+          </div>
+        </div>
+  
+      </div>
+      {/* /.row */}
+  
+    </div>
+    /* /.container */
+  )
+}
+
+export default PageContent;
